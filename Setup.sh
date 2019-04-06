d86ddd3ac27da21bb7d692147df86c520883895c
blue3k <blue3k@gmail.com>
2019-04-05 21:29:59 -0700

    add linux setup script

---
+++ Setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+SF_PATH=$(dirname "$0")
+
+git clone https://github.com/blue3k/ATFClone.git $SF_PATH..\ATF
+
+
+git clone https://github.com/blue3k/StormForge3rdParties.git $SF_PATH/../StormForge3rdParties
+
+ln -s $SF_PATH/../StormForge3rdParties/3rdParties $SF_PATH/3rdParties 
+
+pause
