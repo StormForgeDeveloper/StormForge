0bacc771c86f7a5ff027f384a636508fbfec050b
blue3k <blue3k@gmail.com>
2019-04-09 02:46:36 +0000

    another fix for linux build script

--- CMakeLinuxBuild.sh
+++ CMakeLinuxBuild.sh
@@ -7,20 +7,23 @@ export CMAKE_SYSTEM_NAME=Linux
 
 export BUILD_DIR=build$CMAKE_SYSTEM_NAME
 
-mkdir $BUILD_DIR/Debug
-mkdir $BUILD_DIR/Release
+mkdir $BUILD_DIR/x64Debug
+mkdir $BUILD_DIR/x64Release
 
 cd $BUILD_DIR
 
 
 
 export CMAKE_BUILD_TYPE=Debug
-cd $CMAKE_BUILD_TYPE
-cmake --build .   --target install --config Debug
+cd x64$CMAKE_BUILD_TYPE
+cmake --build .   --target install  --config Debug
+
+
 
 export CMAKE_BUILD_TYPE=Release
-cd ../$CMAKE_BUILD_TYPE
+cd ../x64$CMAKE_BUILD_TYPE
 cmake --build .   --target install --config Release
+
 
 
 echo Done
