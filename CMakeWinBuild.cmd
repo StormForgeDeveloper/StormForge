592be439b766b0cc80283d5a1783ebe49e979fa1
blue3k <blue3k@gmail.com>
2019-04-10 23:44:56 -0700

    fix link error

--- CMakeWinBuild.cmd
+++ CMakeWinBuild.cmd
@@ -11,20 +11,20 @@ set BUILD_DIR=build%CMAKE_SYSTEM_NAME%
 
 set CMAKE_BUILD_TYPE=Debug
 cd %BUILD_DIR%\%PROCESS_ARCHITECTUR%
-cmake --build . --parallel --target install  -- /p:Configuration=Debug 
+cmake --build . --parallel --target install  -- /p:Configuration=%CMAKE_BUILD_TYPE% 
 
 
-robocopy lib ..\%PROCESS_ARCHITECTUR%%CMAKE_BUILD_TYPE%\lib  /purge
-robocopy bin ..\%PROCESS_ARCHITECTUR%%CMAKE_BUILD_TYPE%\bin  /purge
+robocopy lib ..\%PROCESS_ARCHITECTUR%%CMAKE_BUILD_TYPE%\lib  /purge /move
+robocopy bin ..\%PROCESS_ARCHITECTUR%%CMAKE_BUILD_TYPE%\bin  /purge /move
 
 
 
 set CMAKE_BUILD_TYPE=Release
-cmake --build . --parallel --target install -- /p:Configuration=Release 
+cmake --build . --parallel --target install -- /p:Configuration=%CMAKE_BUILD_TYPE% 
 
 
-robocopy lib ..\%PROCESS_ARCHITECTUR%%CMAKE_BUILD_TYPE%\lib  /purge
-robocopy bin ..\%PROCESS_ARCHITECTUR%%CMAKE_BUILD_TYPE%\bin  /purge
+robocopy lib ..\%PROCESS_ARCHITECTUR%%CMAKE_BUILD_TYPE%\lib  /purge /move
+robocopy bin ..\%PROCESS_ARCHITECTUR%%CMAKE_BUILD_TYPE%\bin  /purge /move
 
 
 
