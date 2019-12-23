aa0690d67707a25c7dbfb5037b22ae7d2f9ce878
blue3k <blue3k@gmail.com>
2019-12-22 16:03:58 -0800

    Build openssl and mysql with windows command

--- CMakeWinBuild.cmd
+++ CMakeWinBuild.cmd
@@ -1,11 +1,11 @@
 
 
 set OPENSSL_DIR=3rdParties\src\openssl
-cmd /c %OPENSSL_DIR% & build1.1_Winx64.cmd
+cmd /c "cd %OPENSSL_DIR% & build1.1_Winx64.cmd"
 
 
 set MYSQL_DIR=3rdParties\src\mysql
-cmd /c %MYSQL_DIR% & WinBuild.cmd
+cmd /c "cd %MYSQL_DIR% & WinBuild.cmd"
 
 
 
--- CMakeWinGen.cmd
+++ CMakeWinGen.cmd
@@ -1,9 +1,9 @@
 
 set OPENSSL_DIR=3rdParties\src\openssl
-cmd /c %OPENSSL_DIR% & config1.1_Winx64.cmd
+cmd /c "cd %OPENSSL_DIR% & config1.1_Winx64.cmd"
 
 set MYSQL_DIR=3rdParties\src\mysql
-cmd /c %MYSQL_DIR% & WinGen.cmd
+cmd /c "cd %MYSQL_DIR% & WinGen.cmd"
 
 
 set CMAKE_SYSTEM_NAME=Windows
