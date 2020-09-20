5d074391dee56e1ce889bea9606dcb22ac48cf73
blue3k <blue3k@gmail.com>
2020-09-20 14:37:03 -0700

    Windows build script: Skip build on error

--- CMakeWinBuild.cmd
+++ CMakeWinBuild.cmd
@@ -12,6 +12,7 @@ set CMAKE_BUILD_TYPE=Debug
 cd %BUILD_DIR%\%PROCESS_ARCHITECTUR%
 cmake --build . --parallel --target install  -- /p:Configuration=%CMAKE_BUILD_TYPE% 
 
+if ERRORLEVEL 1 goto exit
 
 robocopy lib ..\%PROCESS_ARCHITECTUR%%CMAKE_BUILD_TYPE%\lib  /purge
 robocopy bin ..\%PROCESS_ARCHITECTUR%%CMAKE_BUILD_TYPE%\bin  /purge
@@ -24,6 +25,7 @@ robocopy Engine\Src\Protocol\%CMAKE_BUILD_TYPE%\ ..\%PROCESS_ARCHITECTUR%%CMAKE_
 set CMAKE_BUILD_TYPE=RelWithDebInfo
 cmake --build . --parallel --target install -- /p:Configuration=%CMAKE_BUILD_TYPE% 
 
+if ERRORLEVEL 1 goto exit
 
 robocopy lib ..\%PROCESS_ARCHITECTUR%%CMAKE_BUILD_TYPE%\lib  /purge
 robocopy bin ..\%PROCESS_ARCHITECTUR%%CMAKE_BUILD_TYPE%\bin  /purge
@@ -34,5 +36,6 @@ robocopy Engine\Src\Protocol\%CMAKE_BUILD_TYPE%\ ..\%PROCESS_ARCHITECTUR%%CMAKE_
 
 cd %~dp0
 
+:exit
 
 
