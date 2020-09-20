87e22e18a2be4482343ad6696e5e40787f69ce20
blue3k <blue3k@gmail.com>
2020-09-20 12:26:04 -0700

    Use shared path for SDKs

--- Setup.cmd
+++ Setup.cmd
@@ -6,7 +6,8 @@ cd %~dp0
 
 
 set SF_PATH=%~dp0
-set SF_3RDPARTY=%SF_PATH%\3rdParties 
+set SF_3RDPARTY=%SF_PATH%3rdParties 
+set SF_SDK=%SF_PATH%..
 set FBX_SDK_INSTALL_FILE_WIN=fbx20201_fbxsdk_vs2017_win.exe
 set VULKAN_SDK_WIN=VulkanSDK-1.2.148.1-Installer.exe
 set TOOL_PATH=%SF_PATH%\3rdParties\tool\bin
@@ -27,19 +28,19 @@ cd %SF_3RDPARTY%\tool
 
 msbuild -m BuildTool.sln /property:Configuration=Release /p:Platform="Any CPU"
 
-cd %SF_3RDPARTY%
+cd %SF_SDK%
 
 if not exist %FBX_SDK_INSTALL_FILE_WIN% (
 	echo downloading FBXSDK
     call %TOOL_PATH%\httpget +url:https://www.autodesk.com/content/dam/autodesk/www/adn/fbx/2020-1/%FBX_SDK_INSTALL_FILE_WIN%
-	%FBX_SDK_INSTALL_FILE_WIN% /D=%SF_3RDPARTY%\FBXSDK
+	%FBX_SDK_INSTALL_FILE_WIN% /D=%SF_SDK%\FBXSDK
 )
 
 
 if not exist %VULKAN_SDK_WIN% (
 	echo downloading VulkanSDK
 	call %TOOL_PATH%\httpget +url:https://sdk.lunarg.com/sdk/download/1.2.148.1/windows/%VULKAN_SDK_WIN%
-	%VULKAN_SDK_WIN% /D=%SF_3RDPARTY%\VulkanSDK
+	%VULKAN_SDK_WIN% /D=%SF_SDK%\VulkanSDK
 )
 
 
--- Setup.sh
+++ Setup.sh
@@ -2,8 +2,6 @@
 
 
 SF_PATH=$(dirname "$0")
-IS_WINDOWS=$(uname -r | grep Microsoft)
-FBX_SDK_INSTALL_FILE_WIN=fbx20201_fbxsdk_vs2017_win.exe
 FBX_SDK_INSTALL_FILE_LINUX=fbx20201_fbxsdk_linux.tar.gz
 
 ATF_PATH=$SF_PATH/../ATF
@@ -18,27 +16,10 @@ fi
 pushd $SF_PATH/3rdParties 
 
 
-if [ ! -f "$FBX_SDK_INSTALL_FILE_WIN" ]; then
-    wget https://www.autodesk.com/content/dam/autodesk/www/adn/fbx/2020-1/$FBX_SDK_INSTALL_FILE_WIN
-fi
-
 # linux version install doesn't work
 #if [ ! -f "$FBX_SDK_INSTALL_FILE_LINUX" ]; then
 #	wget https://www.autodesk.com/content/dam/autodesk/www/adn/fbx/2020-1/fbx20201_fbxsdk_linux.tar.gz
 #fi
-
-
-if [ "$IS_WINDOWS" = "" ]; then
-	echo Running on Linux
-else
-	echo Running on Linux subsystem.
-	
-	if [ ! -d "FBXSDK" ]; then
-		cmd.exe /C "cd > pwd.obj"
-		WINCWD=$(<pwd.obj)
-		cmd.exe /C "$FBX_SDK_INSTALL_FILE_WIN /D=$WINCWD\\FBXSDK"
-	fi
-fi
 
 # linux version install doesn't work
 #tar -xvf fbx20201_fbxsdk_linux.tar.gz -C FBXSDK
@@ -49,24 +30,6 @@ fi
 
 
 # install vulkan SDK
-
-VULKAN_SDK_WIN=VulkanSDK-1.2.148.1-Installer.exe
-if [ ! -f "$VULKAN_SDK_WIN" ]; then
-	wget https://sdk.lunarg.com/sdk/download/1.2.148.1/windows/$VULKAN_SDK_WIN
-fi
-
-
-if [ "$IS_WINDOWS" = "" ]; then
-	echo Running on Linux
-else
-	echo Running on Linux subsystem.
-	if [ ! -d "VulkanSDK" ]; then
-		cmd.exe /C "cd > pwd.obj"
-		WINCWD=$(<pwd.obj)
-		cmd.exe /C "$VULKAN_SDK_WIN /D=$WINCWD\\VulkanSDK"
-	fi
-fi
-
 
 if [ ! -f "/etc/apt/sources.list.d/lunarg-vulkan-1.2.148-bionic.list" ]; then
 	wget -qO - https://packages.lunarg.com/lunarg-signing-key-pub.asc | sudo apt-key add -
