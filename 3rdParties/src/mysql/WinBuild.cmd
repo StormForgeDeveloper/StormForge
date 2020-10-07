94ad9e22c0db02354717dfd1b08b1c3b0284380b
blue3k <blue3k@gmail.com>
2020-10-07 01:38:42 -0700

    Switch to mysql static lib

--- 3rdParties/src/mysql/WinBuild.cmd
+++ 3rdParties/src/mysql/WinBuild.cmd
@@ -6,19 +6,18 @@ set PROCESS_ARCHITECTUR=x64
 
 
 set BUILD_DIR=build%CMAKE_SYSTEM_NAME%
+cd %BUILD_DIR%\%PROCESS_ARCHITECTUR%
 
 
 
 set CMAKE_BUILD_TYPE=Debug
-cd %BUILD_DIR%\%PROCESS_ARCHITECTUR%
 cmake --build . --parallel --target install  -- /p:Configuration=%CMAKE_BUILD_TYPE% 
 
 
 
 set CMAKE_BUILD_TYPE=RelWithDebInfo
-cd %BUILD_DIR%\%PROCESS_ARCHITECTUR%
-cmake --build . --parallel --target install  -- /p:Configuration=%CMAKE_BUILD_TYPE% 
+cmake --build . --parallel --target install  -- /p:Configuration=%CMAKE_BUILD_TYPE%
 
 
 
-
+cd ../../
