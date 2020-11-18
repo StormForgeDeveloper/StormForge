28719b30376a6e923fd128d8ea8761df87732bd5
blue3k <blue3k@gmail.com>
2020-11-17 23:27:34 -0800

    Change 3rdParty linux build path

--- 3rdParties/LinuxBuild.sh
+++ 3rdParties/LinuxBuild.sh
@@ -6,14 +6,12 @@ export CMAKE_SYSTEM_NAME=Linux
 
 export BUILD_DIR=$CMAKE_SYSTEM_NAME
 
-mkdir $BUILD_DIR/x64Debug
-mkdir $BUILD_DIR/x64Release
 
 cd $BUILD_DIR
 
 
 export CMAKE_BUILD_TYPE=Debug
-cd x64$CMAKE_BUILD_TYPE
+cd x64/$CMAKE_BUILD_TYPE
 cmake --build .   --target install  --config Debug
 
 if [ $? -ne 0 ]; then
@@ -22,7 +20,7 @@ fi
 
 
 export CMAKE_BUILD_TYPE=Release
-cd ../x64$CMAKE_BUILD_TYPE
+cd ../../x64/$CMAKE_BUILD_TYPE
 cmake --build .   --target install --config Release
 
 if [ $? -ne 0 ]; then
--- 3rdParties/LinuxGen.sh
+++ 3rdParties/LinuxGen.sh
@@ -8,21 +8,31 @@ export BUILD_DIR=$CMAKE_SYSTEM_NAME
 if [ ! -f $BUILD_DIR ]; then
   mkdir $BUILD_DIR
 fi
-mkdir $BUILD_DIR/x64Debug
-mkdir $BUILD_DIR/x64Release
+
+if [ ! -f $BUILD_DIR/x64 ]; then
+  mkdir $BUILD_DIR/x64
+fi
+
+if [ ! -f $BUILD_DIR/x64/Debug ]; then
+  mkdir $BUILD_DIR/x64/Debug
+fi
+
+if [ ! -f $BUILD_DIR/x64/Release ]; then
+  mkdir $BUILD_DIR/x64/Release
+fi
 
 cd $BUILD_DIR
 
 
 
 export CMAKE_BUILD_TYPE=Debug
-cd $PROCESS_ARCHITECTUR$CMAKE_BUILD_TYPE
-cmake ../../ -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DCMAKE_INSTALL_PREFIX=./    
+cd $PROCESS_ARCHITECTUR/$CMAKE_BUILD_TYPE
+cmake ../../../ -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DCMAKE_INSTALL_PREFIX=./    
 
 
 export CMAKE_BUILD_TYPE=Release
-cd ../$PROCESS_ARCHITECTUR$CMAKE_BUILD_TYPE
-cmake ../../ -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DCMAKE_INSTALL_PREFIX=./   
+cd ../../$PROCESS_ARCHITECTUR/$CMAKE_BUILD_TYPE
+cmake ../../../ -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DCMAKE_INSTALL_PREFIX=./   
 
 
 echo Done
