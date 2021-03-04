1536ac1504d0d2ff477a13f3b915e5b577fbede3
blue3k <blue3k@gmail.com>
2021-03-03 23:10:44 -0800

    Add NO_DATA_EXIST definition to c# code

--- Engine/Src/SFCoreSharp/ResultCode/SFResultCodeSystem.cs
+++ Engine/Src/SFCoreSharp/ResultCode/SFResultCodeSystem.cs
@@ -16,14 +16,16 @@ namespace SF {
 
 		public const int SUCCESS =                              0;
         public const int SUCCESS_FALSE =                        1;
+        public const int SUCCESS_YIELD = 2;
         public const int INVALID_POINTER =                      unchecked((int)0xC0000002L);
-        public const int  FAIL=                                 unchecked((int)0xC0000003L);
-        public const int  NOT_IMPLEMENTED =                     unchecked((int)0xC0000004L);
-        public const int  OUT_OF_MEMORY =                       unchecked((int)0xC0000005L);
-        public const int  UNEXPECTED =                          unchecked((int)0xC0000006L);
+        public const int FAIL=                                 unchecked((int)0xC0000003L);
+        public const int NOT_IMPLEMENTED =                     unchecked((int)0xC0000004L);
+        public const int OUT_OF_MEMORY =                       unchecked((int)0xC0000005L);
+        public const int UNEXPECTED =                          unchecked((int)0xC0000006L);
         public const int INVALID_ARG =                          unchecked((int)0xC0000007L);
-        
-	}
+        public const int NO_DATA_EXIST =                        unchecked((int)0xC0000008L);
+
+    }
 }
 
 
