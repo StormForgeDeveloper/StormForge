96c3d5be79e1ab817cc8c1fbefd10f1fc6b82742
blue3k <blue3k@gmail.com>
2021-07-14 00:58:49 -0700

    Optimize SortedMap

--- Engine/Src/SFCore/Container/SFSortedMap_Base.h
+++ Engine/Src/SFCore/Container/SFSortedMap_Base.h
@@ -27,84 +27,72 @@ namespace SF {
 	{
 	public:
 
-		enum { GrowthBy = 32 };
-
 		using MapNode = MapNodeType;
 		using ReferenceAccessPoint = ReferenceAccessPointType;
 
 	private:
 
-		//ReferenceAccessPoint m_RootDummy;
 		ReferenceAccessPoint* m_Root;
-		StaticArray<const MapNode*, GrowthBy * 2> m_TraversalHistory;
+		int m_HistorySize = 0;
+		const MapNode* m_TraversalHistory[128]{};
 
 	public:
 
 		SortedMapTraversalHistoryT(IHeap& heap, ReferenceAccessPoint& root, size_t totalItemCount)
 			: m_Root(&root)
-			, m_TraversalHistory(heap)
 		{
-			m_TraversalHistory.reserve(Math::CeilLogTwo(totalItemCount + 1));
-			//Assert((size_t)ceil(log2(totalItemCount + 1)) <= m_TraversalHistory.GetAllocatedSize());
-			//m_TraversalHistory.Reserve();
-			m_TraversalHistory.SetIncreaseSize(GrowthBy);
+			assert(Math::CeilLogTwo(totalItemCount + 1) <= countof(m_TraversalHistory));
 		}
 
 		SortedMapTraversalHistoryT(IHeap& heap, const ReferenceAccessPoint& root, size_t totalItemCount)
 			: m_Root(const_cast<ReferenceAccessPoint*>(&root))
-			, m_TraversalHistory(heap)
 		{
-			m_TraversalHistory.reserve(Math::CeilLogTwo(totalItemCount + 1));
-			//Assert((size_t)ceil(log2(totalItemCount + 1)) <= m_TraversalHistory.GetAllocatedSize());
-			//m_TraversalHistory.Reserve();
-			m_TraversalHistory.SetIncreaseSize(GrowthBy);
+			assert(Math::CeilLogTwo(totalItemCount + 1) <= countof(m_TraversalHistory));
 		}
 
-		SF_FORCEINLINE size_t GetHistorySize() const { return m_TraversalHistory.size(); }
+		SF_FORCEINLINE size_t GetHistorySize() const { return m_HistorySize; }
 
-		void Clear()
+		SF_FORCEINLINE void Clear()
 		{
-			m_TraversalHistory.Clear();
+			m_HistorySize = 0;
 		}
 
 		SF_FORCEINLINE void SetPreserveDataOnResize(bool conserveDataOnResize)
 		{
-			m_TraversalHistory.SetPreserveDataOnResize(conserveDataOnResize);
 		}
 
 		SF_FORCEINLINE void AddHistory(const MapNode* pNode)
 		{
-			m_TraversalHistory.push_back(pNode);
+			assert((m_HistorySize+1) < countof(m_TraversalHistory));
+			m_TraversalHistory[m_HistorySize++] = pNode;
 		}
 
 		SF_FORCEINLINE void RemoveLastHistory()
 		{
-			Assert(m_TraversalHistory.size() > 0);
-			m_TraversalHistory.resize(m_TraversalHistory.size() - 1);
+			if (m_HistorySize > 0)
+				m_HistorySize--;
 		}
 
-		SF_FORCEINLINE void TruncateHistoryFrom(int iIndex) { m_TraversalHistory.resize(iIndex); }
+		SF_FORCEINLINE void TruncateHistoryFrom(int iIndex) { assert(iIndex >= 0); m_HistorySize = (size_t)iIndex; }
 
-		SF_FORCEINLINE const MapNode* GetHistory(int iIndex) const { return m_TraversalHistory[iIndex]; }
-		//MapNode* GetHistory(int iIndex) { return m_TraversalHistory[iIndex]; }
+		SF_FORCEINLINE const MapNode* GetHistory(int iIndex) { assert(iIndex >= 0 && iIndex < m_HistorySize); return m_TraversalHistory[iIndex]; }
 
-		SF_FORCEINLINE const MapNode* GetLastHistory() const { if (m_TraversalHistory.size() == 0) return nullptr; return m_TraversalHistory[m_TraversalHistory.size() - 1]; }
-		//MapNode* GetLastHistory() { if (m_TraversalHistory.size() == 0) return nullptr; return m_TraversalHistory[m_TraversalHistory.size() - 1]; }
+		SF_FORCEINLINE const MapNode* GetLastHistory() { if (m_HistorySize == 0) return nullptr; return m_TraversalHistory[m_HistorySize - 1]; }
 
 		// set Reserve size
-		Result Reserve(size_t szReserv)
+		SF_FORCEINLINE Result Reserve(size_t szReserv)
 		{
-			if (szReserv <= m_TraversalHistory.GetAllocatedSize())
-				return ResultCode::SUCCESS;
-
-			szReserv = GrowthBy * ((szReserv + GrowthBy - 1) / GrowthBy);
-
-			return m_TraversalHistory.Reserve(szReserv);
+			assert(szReserv > countof(m_TraversalHistory));
 		}
 
 		SF_FORCEINLINE int FindIndex(const MapNode* pNode) const
 		{
-			return m_TraversalHistory.FindIndex([pNode](const MapNode* x) { return x == pNode; });
+			int iIndex = 0;
+			for (; iIndex < m_HistorySize; iIndex++)
+				if (m_TraversalHistory[iIndex] == pNode)
+					return iIndex;
+
+			return -1;
 		}
 
 		void Replace(uint updateSerial, int nodeIndex, bool isInPlaceSwap, const MapNode* pNode, MapNode* pNewNode)
