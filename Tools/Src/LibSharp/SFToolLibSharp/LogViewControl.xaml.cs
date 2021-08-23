8e09ede58d59e5ea466fce7f1823dc0611fa2f47
blue3k <blue3k@gmail.com>
2021-08-23 00:12:46 -0700

    Adding ssh and log view control to tool lib

---
+++ Tools/Src/LibSharp/SFToolLibSharp/LogViewControl.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Threading;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.Specialized;
+using SF;
+
+namespace SF
+{
+
+
+    public class LogEntry : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            Application.Current.Dispatcher.BeginInvoke((Action)(() =>
+            {
+                PropertyChangedEventHandler handler = PropertyChanged;
+                if (handler != null)
+                    handler(this, new PropertyChangedEventArgs(propertyName));
+            }));
+        }
+
+        public string Message { get; set; }
+    }
+
+
+    /// <summary>
+    /// Interaction logic for LogViewControl.xaml
+    /// </summary>
+    public partial class LogViewControl : UserControl
+    {
+        public ObservableCollection<LogEntry> LogEntries { get; set; }
+
+        public LogViewControl()
+        {
+            InitializeComponent();
+
+            DataContext = LogEntries = new ObservableCollection<LogEntry>();
+        }
+
+
+        private bool AutoScroll = true;
+        private void ScrollViewer_ScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+            // User scroll event : set or unset auto scroll mode
+            if (e.ExtentHeightChange == 0)
+            {   // Content unchanged : user scroll event
+                if ((e.Source as ScrollViewer).VerticalOffset == (e.Source as ScrollViewer).ScrollableHeight)
+                {   // Scroll bar is in bottom
+                    // Set auto scroll mode
+                    AutoScroll = true;
+                }
+                else
+                {   // Scroll bar isn't in bottom
+                    // Unset auto scroll mode
+                    AutoScroll = false;
+                }
+            }
+
+            // Content scroll event : auto scroll eventually
+            if (AutoScroll && e.ExtentHeightChange != 0)
+            {   // Content changed and auto scroll mode set
+                // Auto scroll
+                (e.Source as ScrollViewer).ScrollToVerticalOffset((e.Source as ScrollViewer).ExtentHeight);
+            }
+        }
+
+    }
+}
---
+++ Tools/Src/LibSharp/SFToolLibSharp/SshSessionManager.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SF;
+using SF.Tool;
+using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI;
+using Renci.SshNet;
+
+namespace SF
+{
+    public class SSHSessionmanager
+    {
+        SshClient m_sshClient;
+        List<ForwardedPortLocal> m_PortForwards = new List<ForwardedPortLocal>();
+
+        public SSHSessionmanager()
+        {
+            InitializeSSH();
+        }
+
+        public static SshClient ConnectSsh(
+            string sshHostName, string sshUserName, string sshPassword = null,
+            string sshKeyFile = null, string sshPassPhrase = null, int sshPort = 22)
+        {
+            // check arguments
+            if (string.IsNullOrEmpty(sshHostName))
+                throw new ArgumentException($"{nameof(sshHostName)} must be specified.", nameof(sshHostName));
+            if (string.IsNullOrEmpty(sshHostName))
+                throw new ArgumentException($"{nameof(sshUserName)} must be specified.", nameof(sshUserName));
+            if (string.IsNullOrEmpty(sshPassword) && string.IsNullOrEmpty(sshKeyFile))
+                throw new ArgumentException($"One of {nameof(sshPassword)} and {nameof(sshKeyFile)} must be specified.");
+
+            // define the authentication methods to use (in order)
+            var authenticationMethods = new List<AuthenticationMethod>();
+            if (!string.IsNullOrEmpty(sshKeyFile))
+            {
+                authenticationMethods.Add(new PrivateKeyAuthenticationMethod(sshUserName,
+                    new PrivateKeyFile(sshKeyFile, string.IsNullOrEmpty(sshPassPhrase) ? null : sshPassPhrase)));
+            }
+            if (!string.IsNullOrEmpty(sshPassword))
+            {
+                authenticationMethods.Add(new PasswordAuthenticationMethod(sshUserName, sshPassword));
+            }
+
+            // connect to the SSH server
+            var sshClient = new SshClient(new ConnectionInfo(sshHostName, sshPort, sshUserName, authenticationMethods.ToArray()));
+            sshClient.Connect();
+
+            return sshClient;
+        }
+
+        void InitializeSSH()
+        {
+            try
+            {
+                var sshClient = ConnectSsh(AppConfig.GetValue<string>("SSHServer"),
+                    AppConfig.GetValue<string>("SSHUser"), AppConfig.GetValue<string>("SSHPassword"),
+                    sshPort: AppConfig.GetValue<int>("SSHPort"));
+
+                m_sshClient = sshClient;
+            }
+            catch (Exception exp)
+            {
+                SF.Log.Error("{0} => {1}", exp.Message, exp.StackTrace.ToString());
+            }
+
+        }
+
+        // Add ssh tunneling port. returns forwarded local port
+        public uint AddPortForward(string host, uint port)
+        {
+            if (m_sshClient == null)
+                return 0;
+
+            m_PortForwards
+        }
+    }
+}
