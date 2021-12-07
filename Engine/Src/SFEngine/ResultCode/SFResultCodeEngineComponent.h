053f2a7220fe74c34b8232f83b812c8e385edcce
blue3k <blue3k@gmail.com>
2021-12-07 03:17:06 -0800

    Add missing files

---
+++ Engine/Src/SFEngine/ResultCode/SFResultCodeEngineComponent.h
@@ -0,0 +1,48 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+// 
+// CopyRight (c) Kyungkun Ko
+// 
+// Author : KyungKun Ko
+//
+// Description : Engine result code initializer
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include "SFTypedefs.h"
+#include "Component/SFLibraryComponent.h"
+
+
+namespace SF {
+
+	////////////////////////////////////////////////////////////////////////////////
+	//
+	//	Module
+	//
+
+	class LibraryComponentEngineResultCode : public LibraryComponent
+	{
+	public:
+		static constexpr StringCrc64 TypeName = "LibraryComponentEngineResultCode";
+
+	public:
+
+		LibraryComponentEngineResultCode();
+		~LibraryComponentEngineResultCode();
+
+		virtual const StringCrc64& GetTypeName() override { return TypeName; }
+
+		// Initialize server component
+		virtual Result InitializeComponent() override;
+		// Terminate server component
+		virtual void DeinitializeComponent() override;
+
+	};
+
+}//namespace SF 
+
+
+
+
+
