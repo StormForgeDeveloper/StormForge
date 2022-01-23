e8cbfe9f815519ec8fdef72115e613356bbffea3
blue3k <blue3k@gmail.com>
2022-01-23 00:15:32 -0800

    File grouping in project list

---
+++ Engine/Src/SFCore/Util/SFStringCrcComponent.cpp
@@ -0,0 +1,75 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+// 
+// CopyRight (c) Kyungkun Ko
+// 
+// Author : KyungKun Ko
+//
+// Description : String crc initializer
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include "SFCorePCH.h"
+#include "SFTypedefs.h"
+#include "Util/SFStringCrcComponent.h"
+#include "Service/SFService.h"
+#include "IO/SFFileInputStream.h"
+
+
+namespace SF {
+
+
+	////////////////////////////////////////////////////////////////////////////////
+	//
+	//	Module
+	//
+
+	constexpr StringCrc64 StringCrcLibraryComponent::TypeName;
+
+	StringCrcLibraryComponent::StringCrcLibraryComponent()
+		: LibraryComponent(TypeName)
+	{
+
+	}
+
+	StringCrcLibraryComponent::~StringCrcLibraryComponent()
+	{
+	}
+
+	// Initialize server component
+	Result StringCrcLibraryComponent::InitializeComponent()
+	{
+		Result hr = LibraryComponent::InitializeComponent();
+		if (!hr) return hr;
+
+		const char* inputPaths[] = {
+			"string_list.crcbin",
+			"Contents/string_list.crcbin",
+		};
+
+		for (auto inputPath : inputPaths)
+		{
+			FileInputStream readFrom;
+			if (!readFrom.Open(inputPath))
+				continue;
+
+			SFLog(System, Info, "Loading string crc table! {0}", inputPath);
+
+			auto res = Service::StringDB->LoadStringTable(readFrom);
+			if (!res)
+			{
+				SFLog(System, Error, "Failed to load string crc table! {0}, res:{1}", inputPath, res);
+			}
+		}
+
+		return ResultCode::SUCCESS;
+	}
+
+	// Terminate server component
+	void StringCrcLibraryComponent::DeinitializeComponent()
+	{
+		LibraryComponent::DeinitializeComponent();
+	}
+
+
+}//namespace SF 
+
---
+++ Engine/Src/SFCore/Util/SFStringCrcComponent.h
@@ -0,0 +1,44 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+// 
+// CopyRight (c) Kyungkun Ko
+// 
+// Author : KyungKun Ko
+//
+// Description : Engine string crc initializer
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include "SFTypedefs.h"
+#include "Component/SFLibraryComponent.h"
+
+
+namespace SF {
+
+	////////////////////////////////////////////////////////////////////////////////
+	//
+	//	Module
+	//
+
+	class StringCrcLibraryComponent : public LibraryComponent
+	{
+	public:
+		static constexpr StringCrc64 TypeName = "StringCrcLibraryComponent";
+
+	public:
+
+		StringCrcLibraryComponent();
+		~StringCrcLibraryComponent();
+
+		virtual const StringCrc64& GetTypeName() override { return TypeName; }
+
+		// Initialize server component
+		virtual Result InitializeComponent() override;
+		// Terminate server component
+		virtual void DeinitializeComponent() override;
+
+	};
+
+}//namespace SF 
+
