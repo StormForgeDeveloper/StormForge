2360a5059f2d2d1d3ea8f514891841891237df1a
blue3k <blue3k@gmail.com>
2024-03-02 00:13:30 -0800

    Added missing files

---
+++ Engine/Src/SFProtocolSharp/GameInstancePayloadDataVerify.cs
@@ -0,0 +1,12 @@
+
+namespace SF.Flat.GameInstance
+{
+    static public class PayloadDataVerify
+    {
+        static public bool Verify(Google.FlatBuffers.Verifier verifier, byte typeId, uint tablePos)
+        {
+            return true;
+        }
+    }
+
+}
---
+++ Engine/Src/SFProtocolSharp/GamePayloadDataVerify.cs
@@ -0,0 +1,12 @@
+
+namespace SF.Flat.Game
+{
+    static public class PayloadDataVerify
+    {
+        static public bool Verify(Google.FlatBuffers.Verifier verifier, byte typeId, uint tablePos)
+        {
+            return true;
+        }
+    }
+
+}
