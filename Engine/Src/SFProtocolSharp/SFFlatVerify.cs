83af271c0d2088e537bfd6026217560b599a9d88
blue3k <blue3k@gmail.com>
2024-03-08 12:06:09 -0800

    Updates for flatbuffer 24.3.7

--- Engine/Src/SFProtocolSharp/SFFlatVerify.cs
+++ Engine/Src/SFProtocolSharp/SFFlatVerify.cs
@@ -2,13 +2,4 @@
 namespace SF.Flat
 {
 
-    static public class NamedVariableValueVerify
-    {
-        static public bool Verify(Google.FlatBuffers.Verifier verifier, byte typeId, uint tablePos)
-        {
-            return true;
-        }
-    }
-
-
 }
