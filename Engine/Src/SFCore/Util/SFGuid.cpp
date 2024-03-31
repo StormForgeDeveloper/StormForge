cc35d45847bce8d0f8b8d0820f26f25acbfaef97
blue3k <blue3k@gmail.com>
2024-03-30 17:06:50 -0700

    Fix guid cap handling.

--- Engine/Src/SFCore/Util/SFGuid.cpp
+++ Engine/Src/SFCore/Util/SFGuid.cpp
@@ -44,11 +44,47 @@ namespace SF
         }
 
 
+        bool TryParseRFCSoft(const char* str, Guid& outGuid)
+        {
+            ArrayView<uint8_t> outBuff(16, 0, outGuid.data);
+            Result hr;
+
+            const uint8_t* curPos = reinterpret_cast<const uint8_t*>(str);
+            int basePos = 0;
+            for (int NumDigit : GuidImpl::UUID_NumDigits)
+            {
+                hr = Util::HEXDecode(NumDigit, curPos, outBuff);
+                if (hr.IsFailure())
+                    return false;
+
+                // Swap bytes, UUID uses big endian
+                // for now we only support little endian
+                int numBytes = NumDigit / 2;
+                //GuidImpl::SwapBytes(outBuff, basePos, numBytes);
+
+                curPos += NumDigit;
+                basePos += numBytes;
+
+                // Skip '-'
+                if ((*curPos) == '-')
+                {
+                    curPos++;
+                }
+                else if (numBytes != 1)
+                {
+                    return false;
+                }
+
+            }
+
+            return hr.IsSuccess();
+        }
+
         /*
           Converts a 128-bits unsigned int to an Guid string representation.
           Uses SIMD via Intel's AVX2 instruction set.
          */
-        void m128itos(__m128i x, char* mem)
+        void m128itos(__m128i guid, char* mem)
         {
             // Expand each byte in x to two bytes in res
             // i.e. 0x12345678 -> 0x0102030405060708
@@ -59,10 +95,10 @@ namespace SF
             const __m256i alpha_mask = _mm256_set1_epi8(0x10);
             const __m256i alpha_offset = _mm256_set1_epi8(0x57);
 
-            __m256i a = _mm256_castsi128_si256(x);
-            __m256i as = _mm256_srli_epi64(a, 4);
-            __m256i lo = _mm256_unpacklo_epi8(as, a);
-            __m128i hi = _mm256_castsi256_si128(_mm256_unpackhi_epi8(as, a));
+            __m256i guid_expanded = _mm256_castsi128_si256(guid);
+            __m256i as = _mm256_srli_epi64(guid_expanded, 4);
+            __m256i lo = _mm256_unpacklo_epi8(as, guid_expanded);
+            __m128i hi = _mm256_castsi256_si128(_mm256_unpackhi_epi8(as, guid_expanded));
             __m256i c = _mm256_inserti128_si256(lo, hi, 1);
             __m256i d = _mm256_and_si256(c, mask);
             __m256i alpha = _mm256_slli_epi64(_mm256_and_si256(_mm256_add_epi8(d, add), alpha_mask), 3);
@@ -82,12 +118,12 @@ namespace SF
             *(uint32_t*)(mem + 32) = _mm256_extract_epi32(res, 7);
         }
 
-        // 32 characters are packed into x
-        __m128i StrTom128i(__m256i x)
+        // 32 characters are packed into str_packed. can't handle non RFC CAP case
+        __m128i StrTom128i_Org(__m256i str_packed)
         {
             // Build a mask to apply a different offset to alphas and digits
-            const __m256i sub = _mm256_set1_epi8(0x2F);
-            const __m256i mask = _mm256_set1_epi8(0x20);
+            const __m256i sub = _mm256_set1_epi8(0x2F); // '0' - 1 = 0x2F
+            const __m256i mask = _mm256_set1_epi8(0x20); // 
             const __m256i alpha_offset = _mm256_set1_epi8(0x28);
             const __m256i digits_offset = _mm256_set1_epi8(0x01);
             const __m256i unweave = _mm256_set_epi32(0x0f0d0b09, 0x0e0c0a08, 0x07050301, 0x06040200, 0x0f0d0b09, 0x0e0c0a08, 0x07050301, 0x06040200);
@@ -99,10 +135,51 @@ namespace SF
             // i.e. 0x0102030405060708 -> 0x1002300450067008
             // Horizontal add
             // i.e. 0x1002300450067008 -> 0x12345678
-            __m256i a = _mm256_sub_epi8(x, sub);
+            __m256i a = _mm256_sub_epi8(str_packed, sub);
             __m256i alpha = _mm256_slli_epi64(_mm256_and_si256(a, mask), 2);
             __m256i sub_mask = _mm256_blendv_epi8(digits_offset, alpha_offset, alpha);
             a = _mm256_sub_epi8(a, sub_mask);
+            a = _mm256_shuffle_epi8(a, unweave);
+            a = _mm256_sllv_epi32(a, shift);
+            a = _mm256_hadd_epi32(a, _mm256_setzero_si256());
+            a = _mm256_permute4x64_epi64(a, 0b00001000);
+
+            return _mm256_castsi256_si128(a);
+        }
+
+        // 32 characters are packed into str_packed.
+        __m128i StrTom128i(__m256i str_packed)
+        {
+            // Build a mask to apply a different offset to alphas and digits
+            const __m256i base_sub = _mm256_set1_epi8(0x30); // '0'
+            const __m256i alpha_mask = _mm256_set1_epi8(0x20); // alphabet detection mask
+            const __m256i alpha_any_mask = _mm256_set1_epi8(0x10); // alphabet cap+lower detection mask
+            const __m256i alpha_offset = _mm256_set1_epi8(0x27);
+            const __m256i alpha_cap_offset = _mm256_set1_epi8(0x7);
+            const __m256i digits_offset = _mm256_set1_epi8(0x00);
+            const __m256i unweave = _mm256_set_epi32(0x0f0d0b09, 0x0e0c0a08, 0x07050301, 0x06040200, 0x0f0d0b09, 0x0e0c0a08, 0x07050301, 0x06040200);
+            const __m256i shift = _mm256_set_epi32(0x00000000, 0x00000004, 0x00000000, 0x00000004, 0x00000000, 0x00000004, 0x00000000, 0x00000004);
+
+            // Translate ascii bytes to their value
+            // i.e. 0x3132333435363738 -> 0x0102030405060708
+            // Shift hi-digits
+            // i.e. 0x0102030405060708 -> 0x1002300450067008
+            // Horizontal add
+            // i.e. 0x1002300450067008 -> 0x12345678
+            __m256i base = _mm256_sub_epi8(str_packed, base_sub);
+
+            // make blend make for alphabet lower case
+            __m256i base_alpha_masked = _mm256_and_si256(base, alpha_mask);
+            __m256i alpha = _mm256_slli_epi64(base_alpha_masked, 2);
+
+            // make blend make for alphabet any
+            __m256i base_alpha_any_masked = _mm256_and_si256(base, alpha_any_mask);
+            __m256i alpha_any = _mm256_slli_epi64(base_alpha_any_masked, 3);
+
+            __m256i sub_any_offset = _mm256_blendv_epi8(digits_offset, alpha_cap_offset, alpha_any);
+            __m256i sub_offset = _mm256_blendv_epi8(sub_any_offset, alpha_offset, alpha);
+
+            __m256i a = _mm256_sub_epi8(base, sub_offset);
             a = _mm256_shuffle_epi8(a, unweave);
             a = _mm256_sllv_epi32(a, shift);
             a = _mm256_hadd_epi32(a, _mm256_setzero_si256());
@@ -131,9 +208,6 @@ namespace SF
 
         __m128i ShortFormStrTom128i(const char* mem)
         {
-            // Remove dashes and pack hex ascii bytes in a 256-bits int
-            const __m256i dash_shuffle = _mm256_set_epi32(0x80808080, 0x0f0e0d0c, 0x0b0a0908, 0x06050403, 0x80800f0e, 0x0c0b0a09, 0x07060504, 0x03020100);
-
             __m256i x = _mm256_loadu_si256((__m256i*)mem);
 
             return StrTom128i(x);
@@ -193,35 +267,28 @@ namespace SF
 
     bool Guid::TryParseRFC(const char* str, Guid& outGuid)
     {
-        ArrayView<uint8_t> outBuff(16, 0, outGuid.data);
-        Result hr;
-
-        const uint8_t* curPos = reinterpret_cast<const uint8_t*>(str);
-        int basePos = 0;
-        for (int NumDigit : GuidImpl::UUID_NumDigits)
+        if (CPUInfo::GetFeatures().AVX2)
         {
-            hr = Util::HEXDecode(NumDigit, curPos, outBuff);
-            if (hr.IsFailure())
+            size_t strLen = StrUtil::StringLen(str);
+            if (strLen != 36)
                 return false;
 
-            // Swap bytes, UUID uses big endian
-            // for now we only support little endian
-            int numBytes = NumDigit / 2;
-            GuidImpl::SwapBytes(outBuff, basePos, numBytes);
-
-            curPos += NumDigit;
-            basePos += numBytes;
-
-            // Skip '-'
-            if ((*curPos) != '-')
+            // RFCStrTom128i doesn't check validity. let's run simple and quick test
+            if (str[8] != '-' || str[13] != '-' || str[18] != '-' || str[23] != '-')
+            {
                 return false;
+            }
 
-            curPos++;
-            basePos++;
+            outGuid = Guid(GuidImpl::RFCStrTom128i(str));
+            return true;
+        }
+        else
+        {
+            return GuidImpl::TryParseRFCSoft(str, outGuid);
         }
 
-        return hr.IsSuccess();
     }
+
 
     bool Guid::TryParse32(const char* str, Guid& outGuid)
     {
@@ -229,16 +296,6 @@ namespace SF
         Result hr = Util::HEXDecode(32, reinterpret_cast<const uint8_t*>(str), outBuff);
         if (hr.IsFailure())
             return false;
-
-        // Swap bytes, UUID uses big endian
-        // for now we only support little endian
-        int basePos = 0;
-        for (int numBytes : GuidImpl::UUID_NumBytes)
-        {
-            GuidImpl::SwapBytes(outBuff, basePos, numBytes);
-
-            basePos += numBytes;
-        }
 
         return true;
     }
@@ -282,27 +339,13 @@ namespace SF
         size_t strLen = StrUtil::StringLen(str);
         if (strLen == 36 && StrUtil::Indexof(str, '-')) // assuming that it is RFC format
         {
-            if (CPUInfo::GetFeatures().AVX2)
-            {
-                // RFCStrTom128i doesn't check validity. let's run simple and quick test
-                if (str[8] != '-' || str[13] != '-' || str[18] != '-' || str[23] != '-')
-                {
-                    return false;
-                }
-
-                outGuid = Guid(GuidImpl::RFCStrTom128i(str));
-               return true;
-            }
-            else
-            {
-                return TryParseRFC(str, outGuid);
-            }
+            return TryParseRFC(str, outGuid);
         }
         else if (strLen == 32) // compact format
         {
             if (CPUInfo::GetFeatures().AVX2)
             {
-                outGuid = Guid(GuidImpl::RFCStrTom128i(str));
+                outGuid = Guid(GuidImpl::ShortFormStrTom128i(str));
                 return true;
             }
             else
