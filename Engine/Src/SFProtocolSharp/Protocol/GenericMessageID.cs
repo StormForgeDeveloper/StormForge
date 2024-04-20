24434690bbd56ea6e2b8381e116830c1a40eb995
blue3k <blue3k@gmail.com>
2024-04-20 15:48:31 -0700

    Add missing message id for generic

--- Engine/Src/SFProtocolSharp/Protocol/GenericMessageID.cs
+++ Engine/Src/SFProtocolSharp/Protocol/GenericMessageID.cs
@@ -29,6 +29,18 @@ namespace SF.Net
 		public static readonly MessageID GenericTransactionCmd = MessageID.MakeMessageID(EMessageType.Command, 1, SF.MessageProtocol.Generic, 1);
 		public static readonly MessageID GenericTransactionRes = MessageID.MakeMessageID(EMessageType.Result, 1, SF.MessageProtocol.Generic, 1);
 
+		// Cmd: Message bus send message warpper through gateway. it conveys other message inside
+		public static readonly MessageID MessageBusSendCmd = MessageID.MakeMessageID(EMessageType.Command, 1, SF.MessageProtocol.Generic, 2);
+		public static readonly MessageID MessageBusSendRes = MessageID.MakeMessageID(EMessageType.Result, 1, SF.MessageProtocol.Generic, 2);
+
+		// Cmd: Listen to the message
+		public static readonly MessageID MessageBusListenCmd = MessageID.MakeMessageID(EMessageType.Command, 1, SF.MessageProtocol.Generic, 3);
+		public static readonly MessageID MessageBusListenRes = MessageID.MakeMessageID(EMessageType.Result, 1, SF.MessageProtocol.Generic, 3);
+
+		// Cmd: Post log data through gateway
+		public static readonly MessageID PostLogDataCmd = MessageID.MakeMessageID(EMessageType.Command, 1, SF.MessageProtocol.Generic, 4);
+		public static readonly MessageID PostLogDataRes = MessageID.MakeMessageID(EMessageType.Result, 1, SF.MessageProtocol.Generic, 4);
+
 	}; // public static class MessageIDGeneric
 
 
